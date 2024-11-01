f66db92b10ada3221b30422905cc336af7bad4ba
SACHIN <ghoshsachin2301@gmail.com>
2024-11-01 10:15:49 +0530

    Some changes

--- routes/flightRoutes.js
+++ routes/flightRoutes.js
@@ -47,57 +47,6 @@ router.get("/seat-map", flightSearchController.getSeatMap);
 
 router.get("/baggage-info", flightSearchController.getBaggageInfo);
 
-// // Booking Routes (Protected with auth)
-// router.post(
-//   "/booking/create",
-//   authMiddleware,
-//   validateRequest("flightBooking"),
-//   flightBookingController.createBooking
-// );
-
-// router.get(
-//   "/booking/:bookingId",
-//   authMiddleware,
-//   flightBookingController.getBookingDetails
-// );
-
-// router.post(
-//   "/booking/:bookingId/cancel",
-//   authMiddleware,
-//   flightBookingController.cancelBooking
-// );
-
-// router.get(
-//   "/booking/history",
-//   authMiddleware,
-//   flightBookingController.getBookingHistory
-// );
-
-// // Payment Routes (Protected with auth)
-// router.post(
-//   "/payment/initiate",
-//   authMiddleware,
-//   validateRequest("paymentInitiate"),
-//   flightPaymentController.initiatePayment
-// );
-
-// router.post(
-//   "/payment/verify",
-//   authMiddleware,
-//   flightPaymentController.verifyPayment
-// );
-
-// router.post(
-//   "/payment/refund",
-//   authMiddleware,
-//   flightPaymentController.initiateRefund
-// );
-
-// router.get(
-//   "/payment/:paymentId",
-//   authMiddleware,
-//   flightPaymentController.getPaymentDetails
-// );
 
 module.exports = router;
 
